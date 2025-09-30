6a8f18baf0d0ed613954f8ae5ade9b31c8878b74
Stephan Volynets <svv6@cornell.edu>
2025-09-30 19:49:36 -0400

    Initial Commit

---
+++ a5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  a5
+//
+//  Created by Stephan Volynets on 9/30/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
